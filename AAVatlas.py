e618553fc11fdd68d2fe136a24b018158d440f4e
betegonlab <85507921+betegonlab@users.noreply.github.com>
2025-04-16 13:49:26 -0400

    Update AAVatlas.py

--- AAVatlas.py
+++ AAVatlas.py
@@ -30,7 +30,11 @@ with st.sidebar:
     selected = option_menu("", ["By serotype", 'By cell type'], 
         icons=['virus', 'vignette'], menu_icon="cast", default_index=0)
 
-    st.markdown('######')
+    st.text(' ')
+    st.text(' ')
+    st.text(' ')
+    st.text(' ')
+	st.text(' ')
     st.text("Funded by:")
     st.image("FNIH_logo.svg")
 
