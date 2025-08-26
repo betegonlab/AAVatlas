0a92c85c0247ddce618f4e167fc3a4d71212b433
betegonlab <85507921+betegonlab@users.noreply.github.com>
2025-08-26 15:07:10 -0400

    Update AAVatlas_Class.py

--- AAVatlas_Class.py
+++ AAVatlas_Class.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
-import plotly as px
+import plotly.express as px
 import plotly.graph_objects as go
 import os
+import kaleido
+kaleido.get_chrome_sync()
 class AAVatlas():
 
     def __init__(self):
