f44dedf2e602c5bef6b8ab1fd826535fa8453d88
betegonlab <85507921+betegonlab@users.noreply.github.com>
2025-09-03 11:06:09 -0400

    Fixing infectivityPlot

--- AAVatlas_Class.py
+++ AAVatlas_Class.py
@@ -116,7 +116,10 @@ class AAVatlas():
         infectivityFig = go.Figure()
         try:
             for i in range(13,6,-1):
-                dfs['1E'+str(i)] = pd.read_csv(_self.dataPath+serotype+"/"+serotype+'_subsample_cells_1e'+str(i)+'.txt', delimiter='\t', header=0, names=["Sampled_cells", "Infected"])
+                try:
+                    dfs['1E'+str(i)] = pd.read_csv(_self.dataPath+serotype+"/"+serotype+'_subsample_cells_1e'+str(i)+'.txt', delimiter='\t', header=0, names=["Sampled_cells", "Infected"])
+                except:
+                    continue
                 infectivityFig.add_trace(go.Scatter(x=dfs['1E'+str(i)]["Sampled_cells"], y=dfs['1E'+str(i)]["Infected"], name='1E'+str(i)))
         except:
             return None
